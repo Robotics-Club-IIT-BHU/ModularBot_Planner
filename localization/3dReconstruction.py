cbcf56cbeea4c60e496ce1cba831e4736b88df0d
hex-plex <sendhilsom7@gmail.com>
2020-12-24 06:47:57 +0530

    structure

---
+++ localization/3dReconstruction.py
@@ -0,0 +1,8 @@
+import cv2
+import numpy as np
+
+def get_pos(arr_masks, camera_coor=None, camera_Rot=None):
+    '''
+    This calculates the exact position of the masked object
+    '''
+    return [0,0]
