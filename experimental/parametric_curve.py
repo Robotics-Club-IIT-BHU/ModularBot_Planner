b881dc6a086d7a17fbe99c4f25ea7b1f73215474
hex-plex <sendhilsom7@gmail.com>
2021-02-07 14:35:15 +0530

    Updated ParamPoly2D Class *Contains simple curve generation and plotting *Uses complex numbers

--- experimental/parametric_curve.py
+++ experimental/parametric_curve.py
@@ -1,37 +1,47 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from cmath import polar, rect
-def curve(r, n):
-    diff_angle = 2*np.pi/n
-    init_phasor = rect(r,0)
-    roots = [ (r, i*diff_angle) for i in range(n) ]
-    edge = 2*r*np.sin(diff_angle/2)
-    ds = 0.1/edge
-    s = np.arange(0,n,ds)
-    x, y = [], []
-    for i_s in s:
-        i = np.floor(i_s)
-        tau = i_s - i
-        point = r*rect(1,diff_angle*i)*((1-tau) + tau*rect(1,diff_angle))
-        x.append(point.real)
-        y.append(point.imag)    
-    root_coors = [rect(*root).real for root in roots ],[rect(*root).imag for root in roots ]
-    #points = []
-    #np.polar()
-    return root_coors, (x,y)
-def plot(x,y, points=True):
+class ParamPoly2D:
+    def __init__(self,r ,n):
+        
+        self.r = r
+        self.n = n
+        self.root_coor, self.curve = self.generate_curve()
+
+    def generate_curve(self):
+        
+        self.diff_angle = 2 * np.pi/ self.n
+        self.init_phasor = rect(self.r,0)
+        roots = [ (self.r, i*self.diff_angle) for i in range(self.n) ]
+        self.edge = 2*self.r*np.sin(self.diff_angle/2)
+        ds = 0.1/self.edge
+        self.s = np.arange(0,self.n,ds)
+        x, y = [], []
+        for i_s in self.s:
+            i = np.floor(i_s)
+            tau = i_s - i
+            point = self.r*rect(1,self.diff_angle*i)*((1-tau) + tau*rect(1,self.diff_angle))
+            x.append(point.real)
+            y.append(point.imag)    
+        root_coors = [rect(*root).real for root in roots ],[rect(*root).imag for root in roots ]
+    
+        return root_coors, (x,y)
+    
+    def plot(self,x,y,points=True):
+        
+        plt.plot(x,y, "xb" if points else "-r", label="input")
+        plt.grid(True)
+        plt.axis("equal")
+        plt.xlabel("x[m]")
+        plt.ylabel("y[m]")
+        plt.legend()
+
 
     
-    plt.plot(x,y, "xb" if points else "-r", label="input")
-    plt.grid(True)
-    plt.axis("equal")
-    plt.xlabel("x[m]")
-    plt.ylabel("y[m]")
-    plt.legend()
-
+    
 if __name__ == "__main__":
-    root, curv = curve(50,10)
+    polygon = ParamPoly2D(50,10)
     plt.subplots(1)
-    plot(*root, points=True)
-    plot(*curv, points=False)    
+    polygon.plot(*polygon.root_coor, points=True)
+    polygon.plot(*polygon.curve, points=False)    
     plt.show()
