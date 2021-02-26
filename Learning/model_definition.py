9314227364c66102f11e75ebdac36dfb60042d74
hex-plex <sendhilsom7@gmail.com>
2021-02-26 23:19:35 +0530

    Basic Actor Critic > *Contains simple class definition > *Based on A3C > *Works with CNN and LSTM

--- Learning/model_definition.py
+++ Learning/model_definition.py
@@ -16,6 +16,15 @@ def weights_init(m):
         fan_in = np.prod(weight_shape[1:4])
         fan_out = np.prod(weight_shape[2:4]) * weight_shape[0]
         w_bound = np.sqrt(6. / (fan_in + fan_out))
+        m.weight.data.uniform_(-w_bound, w_bound)
+        m.bias.data.fill_(0)
+    elif classname.find('Linear') != -1:
+        weight_shape = list(m.weight.data.size())
+        fan_in = weight_shape[1]
+        fan_out = weight_shape[0]
+        w_bound = np.sqrt(6. / (fan_in + fan_out))
+        m.weight.data.uniform_(-w_bound, w_bound)
+        m.bias.data.fill_(0)
 
 class Actor(nn.Module):
 
@@ -33,4 +42,29 @@ class Actor(nn.Module):
         self.actor_linear = nn.Linear(256, num_outputs)
 
         self.apply(weights_init)
-        self.actor_linear.weight.data = normalized_c
+        self.actor_linear.weight.data = normalized_columns_initializer(
+            self.actor_linear.weight.data, 0.01
+        )
+        self.actor_linear.bias.data.fill_(0)
+        self.critic_linear.weight.data = normalized_columns_initializer(
+            self.critic_linear.weight.data, 1.0
+        )
+        self.critic_linear.bias.data,fill_(0)
+
+        self.lstm.bias_ih.data.fill_(0)
+        self.lstm.bias_hh.data.fill_(0)
+
+        self.train()
+
+    def forward(self, inputs):
+    inputs, (hx, cx) = inputs
+    x = F.elu(self.conv1(inputs))
+    x = F.elu(self.conv2(x))
+    x = F.elu(self.conv3(x))
+    x = F.elu(self.conv4(x))
+
+    x = x.view(-1, 32*32*3)
+    hx, cx = self.lstm(x, (hx, cx))
+    x = hx
+
+    return self.critic_linear(x), self.actor_linear(x), (hx, cx)
